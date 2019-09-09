11318a3df32036ff27fda22a48fbdd0b2a7ae077
Charlie Paisan <cppaisan@gmail.com>
2019-09-08 23:31:26 -0700

    [FEATURE] Allow user to search Medium for a publication's content - Clicking submit makes a request to the API for the Medium feed - Display each of the publication's articles in Cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
@@ -10,31 +13,59 @@ const useStyles = makeStyles({
       "nav"
       "content"
     `,
-    gridTemplateColumns: "auto",
     padding: 32
   },
   nav: {
     gridArea: "nav",
-    display: "flex",
-    placeSelf: "center",
-    width: "max-content",
-    justifyContent: "space-around"
+    placeSelf: "center"
   },
   searchInput: {
     width: 300,
     marginRight: 16
+  },
+  content: {
+    marginTop: 16,
+    gridArea: "content",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    "& img": {
+      maxWidth: "100%"
+    }
+  },
+  card: {
+    height: "max-content",
+    marginBottom: 16,
+    marginLeft: 8,
+    marginRight: 8,
+    maxWidth: "32%"
   }
 });
+
+const API_URL = "http://localhost:3001/medium";
+const DATE_OPTIONS = {
+  day: "numeric",
+  month: "short",
+  year: "numeric"
+};
 
 function App() {
   const classes = useStyles();
   const [searchText, setSearchText] = useState("");
+  const [feed, setFeed] = useState([]);
 
   const onSearchChange = ({ target: { value } }) => setSearchText(value);
 
   const requestMediumFeed = async () => {
-    // TODO: make API request
+    try {
+      const apiResponse = await fetch(`${API_URL}/${searchText}`);
+      const { items = [] } = await apiResponse.json();
+      setFeed(items);
+    } catch (e) {
+      // TODO: display error
+    }
   };
+
   return (
     <div className={classes.root}>
       <div className={classes.nav}>
@@ -48,6 +79,22 @@ function App() {
           Search
         </Button>
       </div>
+      {feed.length > 0 && (
+        <div className={classes.content}>
+          {feed.map(({ title = "", isoDate = null, content }) => (
+            <Card key={title} className={classes.card}>
+              <CardContent>
+                <Typography variant="h5">{title}</Typography>
+                <Typography variant="h6">
+                  Published:{" "}
+                  {new Date(isoDate).toLocaleDateString("en-US", DATE_OPTIONS)}
+                </Typography>
+                <div dangerouslySetInnerHTML={{ __html: content }} />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
