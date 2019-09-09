82dfc4a92dc388eb73cb84ed6ba37bb432dbfc68
Charlie Paisan <cppaisan@gmail.com>
2019-09-08 21:56:44 -0700

    [FEATURE] Add text field and search button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+    gridTemplateAreas: `
+      "nav"
+      "content"
+    `,
+    gridTemplateColumns: "auto",
+    padding: 32
+  },
+  nav: {
+    gridArea: "nav",
+    display: "flex",
+    placeSelf: "center",
+    width: "max-content",
+    justifyContent: "space-around"
+  },
+  searchInput: {
+    width: 300,
+    marginRight: 16
+  }
+});
 
 function App() {
+  const classes = useStyles();
+  const [searchText, setSearchText] = useState("");
+
+  const onSearchChange = ({ target: { value } }) => setSearchText(value);
+
+  const requestMediumFeed = async () => {
+    // TODO: make API request
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={classes.nav}>
+        <TextField
+          className={classes.searchInput}
+          placeholder="Search Medium..."
+          value={searchText}
+          onChange={onSearchChange}
+        />
+        <Button variant="contained" color="primary" onClick={requestMediumFeed}>
+          Search
+        </Button>
+      </div>
     </div>
   );
 }
